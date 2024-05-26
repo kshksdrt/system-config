a49693f664ba820ddfe860a0b11688b51dba075d
kshksdrt <kaushik.sdrt@gmail.com>
2024-05-26 22:41:19 +0200

    Update powershell splash screen

--- windows/PowershellSplashScreen.ps1
+++ windows/PowershellSplashScreen.ps1
@@ -1,13 +1,24 @@
 Write-Host "Select a shell to open:"
-Write-Host "1) PowerShell"
-Write-Host "2) Cmd"
-Write-Host "3) WSL Ubuntu"
+Write-Host "[1] PowerShell"
+Write-Host "[2] Cmd"
+Write-Host "[3] WSL Ubuntu"
 
-$choice = Read-Host "Enter your choice [1-4]"
+$key = [console]::ReadKey($true).Key
 
-switch ($choice) {
-    1 { pwsh }
-    2 { cmd }
-    3 { wsl }
-    default { Write-Host "Invalid choice" }
+switch ($key) {
+    'D1' { 
+        Clear-Host
+        pwsh
+    }
+    'D2' { 
+        Clear-Host
+        cmd
+    }
+    'D3' { 
+        Clear-Host
+        wsl 
+    }
+    default { 
+        Write-Host "Invalid choice" 
+    }
 }
